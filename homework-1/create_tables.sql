fe0b597450fdd4f43f630f60e768b38f008232b3
Ayrat Fatykhov <airat_91_91@mail.ru>
2024-01-11 19:09:23 +0300

    add sql code

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,23 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees (
+employee_id int PRIMARY KEY,
+first_name varchar(100) NOT NULL,
+last_name varchar(100) NOT NULL,
+title varchar(255) NOT NULL,
+birth_date varchar(100) NOT NULL,
+notes text
+);
+
+CREATE TABLE customers_v2 (
+customer_id varchar(5) PRIMARY KEY,
+company_name varchar(100) NOT NULL,
+contact_name varchar(100) NOT NULL
+);
+
+CREATE TABLE orders (
+order_id int PRIMARY KEY,
+customer_id varchar(5) REFERENCES customers_v2(customer_id) NOT NULL,
+employee_id int REFERENCES employees(employee_id) NOT NULL,
+order_date varchar(100) NOT NULL,
+ship_city varchar(100) NOT NULL
+);
